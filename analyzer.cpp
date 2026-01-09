fa255e826b6e841cdc6bdb640cfe8b8fdbd0ded5
azradyglr <azra.dayiogullari@bahcesehir.edu.tr>
2026-01-09 22:34:23 +0300

    Update analyzer.cpp

--- analyzer.cpp
+++ analyzer.cpp
@@ -18,6 +18,8 @@ void TripAnalyzer:: ingestFile(const std::string& csvPath) {
    // - skip malformed rows
    // - extract PickupZoneID and pickup hour
    // - aggregate counts  
+   std::ios_base::sync_with_stdio(false);
+   std::cin.tie(NULL);
 	
    std::ifstream file(csvPath);
    if (!file.is_open()) return; //dosya açılmazsa bir şey yapma
@@ -111,6 +113,7 @@ std::vector<SlotCount> TripAnalyzer::topBusySlots(int k) const {
    }
    return slots;
 }
+
 
 
 
